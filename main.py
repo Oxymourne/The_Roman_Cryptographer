fc48d8da6c2c63001f25385049ecc33d277201ad
Макс <kingmaks90@yandex.ru>
2024-12-04 01:54:26 +0300

    1

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ original_text = ''
 step = ''
 
 
-def input_text():  # Забираем значения из поля ввода
+def encryption_text():  # Забираем значения из поля ввода шифруем и передаем в поле вывода
     global original_text, step
+    original_text = ''
     flag = True
     step = step_field.get().strip()
     if step == '':
@@ -23,11 +24,31 @@ def input_text():  # Забираем значения из поля ввода
         error_label.place(x=500, y=550)
 
 
+def decryption_text():  # Забираем значения из поля ввода дешифруем и передаем в поле вывода
+    global original_text, step
+    original_text = ''
+    flag = True
+    step = step_field.get().strip()
+    if step == '':
+        flag = False
+    for digit in step:
+        if not digit.isdigit():
+            flag = False
+    if flag is True:
+        step = int(step)
+        original_text = input_field.get("1.0", "end").strip()
+        output_field.delete("1.0", "end")
+        output_field.insert("1.0", decryption(original_text, step))
+        error_label.place_forget()
+    else:
+        error_label.place(x=500, y=550)
+
+
 def clear_input():  # Очистка окна ввода
     input_field.delete("1.0", "end")
 
 
-def encryption(text, encryption_step):
+def encryption(text, encryption_step):  # Функция Шифрования
     new_text = ''
     for i in range(len(text)):
         ascii_code = ord(text[i])
@@ -36,11 +57,34 @@ def encryption(text, encryption_step):
                 new_text += chr(ascii_code - 95 + encryption_step)
             else:
                 new_text += chr(ascii_code + encryption_step)
-        if 1040 <= ascii_code <= 1103:  # Условие нахождения в Русской части символов
+        elif 1040 <= ascii_code <= 1103:  # Условие нахождения в Русской части символов
             if ascii_code + encryption_step > 1103:
                 new_text += chr(ascii_code - 64 + encryption_step)
             else:
                 new_text += chr(ascii_code + encryption_step)
+        else:
+            new_text += text[i]
+
+    return new_text
+
+
+'''Поправит функцию. Если код - шаг меньше 32 выдает пустоту'''
+
+
+def decryption(text, decryption_step):  # Функция Дешифрования
+    new_text = ''
+    for i in range(len(text)):
+        ascii_code = ord(text[i])
+        if 32 <= ascii_code <= 126:  # Условие нахождения символа в таблице символов и инагл алфавита
+            if ascii_code - decryption_step < 33:
+                new_text += chr(ascii_code + 94 - decryption_step)
+            else:
+                new_text += chr(ascii_code - decryption_step)
+        elif 1040 <= ascii_code <= 1103:  # Условие нахождения в Русской части символов
+            if ascii_code - decryption_step < 1040:
+                new_text += chr(ascii_code + 64 - decryption_step)
+            else:
+                new_text += chr(ascii_code - decryption_step)
         else:
             new_text += text[i]
 
@@ -55,12 +99,9 @@ window.resizable(False, False)
 # Кнопки
 button_clear = tk.Button(window, text='Очистить', width=12, height=2, command=clear_input)  # Кнопка очистки поля ввода
 button_clear.place(x=950, y=525)
-'''
-button_eng = tk.Button(window, text='EN')  # Кнопка Английского языка
-button_eng.place(x=200, y=425)'''
-button_encrypt = tk.Button(window, text='Шифровать', width=12, height=2, command=input_text)  # Кнопка шифрования
+button_encrypt = tk.Button(window, text='Шифровать', width=12, height=2, command=encryption_text)  # Кнопка шифрования
 button_encrypt.place(x=100, y=525)
-button_decrypt = tk.Button(window, text='Дешифровать', width=12, height=2, command=input_text)  # Кнопка дешифрования
+button_decrypt = tk.Button(window, text='Дешифровать', width=12, height=2, command=decryption_text)  # Кнопка дешифрования
 button_decrypt.place(x=1090, y=525)
 
 # Текстовые блоки
