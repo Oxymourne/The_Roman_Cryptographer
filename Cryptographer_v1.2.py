cf2845d68727d4fed5d1e6998aa9096bcd2e48b3
Макс <kingmaks90@yandex.ru>
2024-12-15 00:59:09 +0300

    Оптимизировал код: 1. При нажатии на шифровать и дешифровать теперь вызывается только 1 общая функция 2. Добавил флаг для шифровки и дешифровки. Если флаг True то в алгоритм шифрования передается -step 3. Алгоритм шифрования теперь тоже общий. Действие зависит от того, какой шаг передается

--- Cryptographer_v1.2.py
+++ Cryptographer_v1.2.py
@@ -1,73 +1,44 @@
 import tkinter as tk
 
-original_text = ''
-step = ''
+
+def decryption_true():
+    global decryption_flag
+    decryption_flag = True
+
+
+def decryption_false():
+    global decryption_flag
+    decryption_flag = False
+
+
+def call_functions_encryption():
+    decryption_false()
+    encryption_text()
+
+
+def call_functions_decryption():
+    decryption_true()
+    encryption_text()
 
 
 def encryption_text():  # Забираем значения из поля ввода шифруем и передаем в поле вывода
-    global original_text, step
+    global original_text, decryption_flag
     original_text = ''
-    flag = True
-    step = step_field.get().strip()
-    if step == '':
-        flag = False
-    if step[0] == '-':
-        straight_step = False
-        for i in range(1, len(step)):
-            if not step[i].isdigit():
-                flag = False
-    else:
-        straight_step = True
-        for i in range(len(step)):
-            if not step[i].isdigit():
-                flag = False
-    if flag is True and straight_step is True:
-        step = int(step)
-        original_text = input_field.get("1.0", "end").strip()
-        output_field.delete("1.0", "end")
-        output_field.insert("1.0", encryption(original_text, step))
-        error_label.place_forget()
-    elif flag is True and straight_step is False:
-        step = abs(int(step))
-        original_text = input_field.get("1.0", "end").strip()
-        output_field.delete("1.0", "end")
-        output_field.insert("1.0", decryption(original_text, step))
-        error_label.place_forget()
-    else:
+    try:
+        step = int(step_field.get().strip())
+    except:
         error_label.place(x=335, y=325)
-
-
-def decryption_text():  # Забираем значения из поля ввода дешифруем и передаем в поле вывода
-    global original_text, step
-    original_text = ''
-    flag = True
-    step = step_field.get().strip()
-    if step == '':
-        flag = False
-    if step[0] == '-':
-        straight_step = False
-        for i in range(1, len(step)):
-            if not step[i].isdigit():
-                flag = False
     else:
-        straight_step = True
-        for i in range(len(step)):
-            if not step[i].isdigit():
-                flag = False
-    if flag is True and straight_step is True:
-        step = int(step)
-        original_text = input_field.get("1.0", "end").strip()
-        output_field.delete("1.0", "end")
-        output_field.insert("1.0", decryption(original_text, step))
-        error_label.place_forget()
-    elif flag is True and straight_step is False:
-        step = abs(int(step))
-        original_text = input_field.get("1.0", "end").strip()
-        output_field.delete("1.0", "end")
-        output_field.insert("1.0", encryption(original_text, step))
-        error_label.place_forget()
-    else:
-        error_label.place(x=335, y=325)
+        if decryption_flag is False:
+            original_text = input_field.get("1.0", "end").strip()
+            output_field.delete("1.0", "end")
+            output_field.insert("1.0", encryption(original_text, step))
+            error_label.place_forget()
+        else:
+            original_text = input_field.get("1.0", "end").strip()
+            output_field.delete("1.0", "end")
+            output_field.insert("1.0", encryption(original_text, -step))
+            error_label.place_forget()
 
 
 def clear_input():  # Очистка окна ввода
@@ -89,19 +60,8 @@ def encryption(text, encryption_step):  # Функция Шифрования
     return new_text
 
 
-def decryption(text, decryption_step):  # Функция Дешифрования
-    new_text = ''
-    for i in range(len(text)):
-        ascii_code = ord(text[i])
-        if 32 <= ascii_code <= 126:  # Условие нахождения символа в таблице символов и инагл алфавита
-            new_text += chr(((ascii_code - 32 - decryption_step) % 95 + 95) % 95 + 32)
-        elif 1040 <= ascii_code <= 1103:  # Условие нахождения в Русской части символов
-            new_text += chr(((ascii_code - 1040 - decryption_step) % 64 + 64) % 64 + 1040)
-        else:
-            new_text += text[i]
-
-    return new_text
-
+original_text = ''
+decryption_flag = True
 
 window = tk.Tk()
 window.title('Римский шифратор')
@@ -111,10 +71,11 @@ window.resizable(False, False)
 # Кнопки
 button_clear = tk.Button(window, text='Очистить', width=12, height=2, command=clear_input)  # Кнопка очистки поля ввода
 button_clear.place(x=550, y=300)
-button_encrypt = tk.Button(window, text='Шифровать', width=12, height=2, command=encryption_text)  # Кнопка шифрования
+button_encrypt = tk.Button(window, text='Шифровать', width=12, height=2,
+                           command=call_functions_encryption)  # Кнопка шифрования
 button_encrypt.place(x=50, y=300)
 button_decrypt = tk.Button(window, text='Дешифровать', width=12, height=2,
-                           command=decryption_text)  # Кнопка дешифрования
+                           command=call_functions_decryption)  # Кнопка дешифрования
 button_decrypt.place(x=657, y=300)
 
 # Текстовые блоки
